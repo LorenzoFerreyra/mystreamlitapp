d91a6d3c9ffd36d70cfff656918c36c0e8e356ca
Lorenzo Ferreyra <ferreyralorenzo2@gmail.com>
2023-12-17 18:34:50 -0300

    Contact Form Added

--- pages/4_💬_Contact.py
+++ pages/4_💬_Contact.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 st.write(
     f'<div style="text-align: center;"><h1>Contact</h1></div>',
     unsafe_allow_html=True
@@ -20,3 +21,27 @@ with column2:
 with column3:
     st.image("assets/git.png", width=50)
     st.write("[GitHub](https://github.com/LorenzoFerreyra)")
+
+
+st.header(":mailbox: Get in touch with me!")
+
+
+contact_form = """
+<form action="https://formsubmit.co/332d019338e1abc0ad41921a21a97e07" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
+
+# Use Local CSS File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")
