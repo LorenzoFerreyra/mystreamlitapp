4f4462b864c71a2614f1de49a589861ac6ca3b9f
Lorenzo Ferreyra <ferreyralorenzo2@gmail.com>
2023-12-17 17:53:34 -0300

    Section Projects completed
    And changes on look & feel

--- pages/3_📚_Projects.py
+++ pages/3_📚_Projects.py
@@ -6,3 +6,11 @@ st.write(
 )
 st.divider()
 st.markdown("#### Things I worked on")
+
+# Proyecto 1
+st.image("assets\social_data.jpg", caption="Shiny App for analyzing social data in R", use_column_width=True)
+st.markdown("[GitHub Repo](https://github.com/LorenzoFerreyra/shinyapp)")
+
+# Proyecto 2
+st.image("assets\is-crypto-dead.jpeg", caption="Crypto Tracker Web App in Python + Django", use_column_width=True)
+st.markdown("[GitHub Repo](https://github.com/esgaelramos/YourMoneyTrackerMail) | [Sitio web](https://mymoneyup.tech/)")
