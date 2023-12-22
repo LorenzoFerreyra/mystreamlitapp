8f58261bb7af17dc687e94b6e7a7901917140cef
Lorenzo Ferreyra <ferreyralorenzo2@gmail.com>
2023-12-22 17:28:49 -0300

    Create 5_🎉_Notebooks.py

---
+++ pages/5_🎉_Notebooks.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+st.write(
+    f'<div style="text-align: center;"><h1>My notebooks</h1></div>',
+    unsafe_allow_html=True
+)
+st.divider()
+
+
+st.write(
+    """
+    Welcome to my Notebooks! Below, you can explore various data science projects 
+    that I've worked on. Each project is a notebook showcasing different aspects 
+    of data analysis, data wrangling, statistics, and more.
+
+    Click on the project titles below to dive into the details:
+
+    - [Meet Dr. Ignaz Semmelweis](https://lorenzoferreyra.github.io/notebook1.html)
+    - [Scala's real-world project repository data](https://lorenzoferreyra.github.io/notebook2.html)
+    - [Data Wrangling](https://lorenzoferreyra.github.io/notebook3.html)
+    - [The most Nobel of Prizes](https://lorenzoferreyra.github.io/notebook4.html)
+    - [Car Insurance Analysis](https://lorenzoferreyra.github.io/notebook5.html)
+    - [SAT Examination](https://lorenzoferreyra.github.io/notebook6.html)
+    - [Bank Economy](https://lorenzoferreyra.github.io/notebook7.html)
+    - [Netflix and Streaming](https://lorenzoferreyra.github.io/notebook8.html)
+    - [Google Play Store apps and reviews](https://lorenzoferreyra.github.io/notebook9.html)
+    - [The oldest businesses in the world](https://lorenzoferreyra.github.io/notebook10.html)
+    - [The movies](https://lorenzoferreyra.github.io/notebook11.html)
+
+    Feel free to explore, and if you have any questions or feedback, don't hesitate to reach out!
+        """
+    )
